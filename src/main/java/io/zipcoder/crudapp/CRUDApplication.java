7830a896208418268932742ada562409388a8d3a
David Ginzberg <arguing.with.my.keyboard@gmail.com>
2017-06-15 09:08:09 -0400

    first commit

---
+++ src/main/java/io/zipcoder/crudapp/CRUDApplication.java
@@ -0,0 +1,22 @@
+package io.zipcoder.crudapp;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CRUDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CRUDApplication.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean h2servletRegistration(){
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
+}
