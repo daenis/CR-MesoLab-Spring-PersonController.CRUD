3ec34de57a5d64ca22c55663e31ac5ea9ad24314
dennis <denniskalaygian@gmail.com>
2018-01-27 12:29:55 -0800

    Built person model

---
+++ src/main/java/io/zipcoder/crudapp/model/Person.java
@@ -0,0 +1,42 @@
+package io.zipcoder.crudapp.model;
+
+public class Person {
+
+    private int id;
+    private int firstName;
+    private int lastName;
+
+    public Person() {
+    }
+
+    public Person(int id, int firstName, int lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(int firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(int lastName) {
+        this.lastName = lastName;
+    }
+
+}
