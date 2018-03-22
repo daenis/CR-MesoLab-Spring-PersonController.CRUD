ba888f9043319b9933141dac0f368ad5d51c0788
dennis <denniskalaygian@gmail.com>
2018-03-21 17:32:13 -0700

    Added description

--- src/main/java/io/zipcoder/crudapp/annotation/AppController.java
+++ src/main/java/io/zipcoder/crudapp/annotation/AppController.java
@@ -8,6 +8,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Custom annotation to be placed on controller. Every controller will need the
+   @RestController annotation, and every controller's endpoint mapping will begin
+   with "/api". Therefore, we can create an annotation to handle these configurations
+   for us.
+ * */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @RestController
